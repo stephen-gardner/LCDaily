ebfa82a3d57c13bacf019e7c68087d4dfe7cd90b
Stephen Gardner <stephenbgardner@gmail.com>
2022-11-22 18:27:40 -0800

    0036. with bits

--- 0036_valid_sudoku/main.go
+++ 0036_valid_sudoku/main.go
@@ -1,23 +1,23 @@
 // Problem: https://leetcode.com/problems/valid-sudoku/
-// Results: https://leetcode.com/submissions/detail/848336369/
+// Results: https://leetcode.com/submissions/detail/848345362/
 package main
 
 func isValidSudoku(board [][]byte) bool {
-	var row, col, box [9][9]bool
+	var row, col, box [9]int
 	for y := 0; y < 9; y++ {
 		for x := 0; x < 9; x++ {
-			c := board[y][x]
+			c := int(board[y][x])
 			if c == '.' {
 				continue
 			}
-			c -= '1'
+			c = 1 << (c - '1')
 			boxNum := ((y / 3) * 3) + x/3
-			if row[y][c] || col[x][c] || box[boxNum][c] {
+			if row[y]&c != 0 || col[x]&c != 0 || box[boxNum]&c != 0 {
 				return false
 			}
-			row[y][c] = true
-			col[x][c] = true
-			box[boxNum][c] = true
+			row[y] |= c
+			col[x] |= c
+			box[boxNum] |= c
 		}
 	}
 	return true
